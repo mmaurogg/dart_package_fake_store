dd679765386fcff50c4b226749d2dc130cf975cf
Manuel Mauricio Gomez Gallo <mmaurogg@Mac-mini-de-Manuel.local>
2025-09-29 14:33:58 -0500

    strat project

---
+++ example/fake_store_example.dart
@@ -0,0 +1,6 @@
+import 'package:fake_store/fake_store.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/fake_store.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/fake_store_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
